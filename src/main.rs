ffab5c2d6ef40f7286dd700a3c9141c89a164e19
ag-dragon <h28941+github@gmail.com>
2023-10-03 09:52:58 -0400

    Rocket running with test template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use rocket::fs::{FileServer, relative};
+use rocket_dyn_templates::{Template, context};
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", context! {
+    })
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .attach(Template::fairing())
+        .mount("/", FileServer::from(relative!("static")))
+        .mount("/", routes![index])
 }
