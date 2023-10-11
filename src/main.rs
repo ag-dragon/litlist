c6469cc3a052a981b74c000c7a49d029060c16e3
ag-dragon <h28941+github@gmail.com>
2023-10-11 12:57:23 -0400

    Start changing entry to story and fix link to story page

--- src/main.rs
+++ src/main.rs
@@ -34,56 +34,56 @@ pub struct Lit {
 }
 
 #[get("/create")]
-fn create_entry() -> Flash<Redirect> {
+fn create_story() -> Flash<Redirect> {
     use schema::entries;
 
-    let new_entry = self::models::NewEntry { title: "test_entry" };
+    let new_story = self::models::NewEntry { title: "test_story" };
 
     let connection = &mut establish_connection();
     diesel::insert_into(entries::table)
-        .values(&new_entry)
+        .values(&new_story)
         .returning(Entry::as_returning())
         .get_result(connection)
-        .expect("Error saving new entry");
+        .expect("Error saving new story");
     Flash::success(Redirect::to("/"), "Entry successfully created.")
 }
 
-#[delete("/<entry_id>")]
-fn delete_entry(entry_id: i32) -> Flash<Redirect> {
+#[delete("/<story_id>")]
+fn delete_story(story_id: i32) -> Flash<Redirect> {
     use self::schema::entries::dsl::*;
 
     let connection = &mut establish_connection();
-    diesel::delete(entries.filter(id.eq(entry_id)))
+    diesel::delete(entries.filter(id.eq(story_id)))
         .execute(connection)
         .expect("Error deleting entries");
 
     Flash::success(Redirect::to("/"), "Entry successfully deleted.")
 }
 
-#[get("/entry/<entry_id>")]
-fn entry(entry_id: i32) -> Template {
+#[get("/stories/<story_id>")]
+fn story(story_id: i32) -> Template {
     use self::schema::entries::dsl::*;
 
     let connection = &mut establish_connection();
 
     let mut query_result = entries
-        .filter(id.eq(entry_id))
+        .filter(id.eq(story_id))
         .select(Entry::as_select())
         .load(connection)
         .expect("Error loading entries");
 
-    let entry_o = query_result.pop();
-    let entry: Entry;
-    match entry_o {
-        Some(v) => entry = v,
-        _ => return Template::render("entry", context! {}),
+    let story_o = query_result.pop();
+    let story: Entry;
+    match story_o {
+        Some(v) => story = v,
+        _ => return Template::render("story", context! {}),
     }
     let lit = Lit {
-        id: entry.id,
-        title: entry.title,
+        id: story.id,
+        title: story.title,
     };
 
-    Template::render("entry", context! { lit })
+    Template::render("story", context! { lit })
 }
 
 #[get("/")]
@@ -99,8 +99,8 @@ fn index() -> Template {
         .expect("Error loading entries");
 
     let mut lits: Vec<Lit> = Vec::new();
-    for entry in query_result {
-        lits.push(Lit { id: entry.id, title: entry.title });
+    for story in query_result {
+        lits.push(Lit { id: story.id, title: story.title });
     }
 
     Template::render("index", context! { lits })
@@ -113,8 +113,8 @@ fn rocket() -> _ {
         .mount("/", FileServer::from(relative!("static")))
         .mount("/", routes![
             index,
-            create_entry,
-            delete_entry,
-            entry,
+            create_story,
+            delete_story,
+            story,
         ])
 }
