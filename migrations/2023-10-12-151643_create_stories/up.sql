db771f75976617089138b21358a9e36a7239156b
ag-dragon <h28941+github@gmail.com>
2023-10-16 09:55:27 -0400

    Change migration/schema to have more columns

--- migrations/2023-10-12-151643_create_stories/up.sql
+++ migrations/2023-10-12-151643_create_stories/up.sql
@@ -1,4 +1,10 @@
 CREATE TABLE stories (
     id SERIAL PRIMARY KEY,
-    title VARCHAR NOT NULL
+    title VARCHAR NOT NULL,
+    author VARCHAR NOT NULL,
+    rating INTEGER,
+    comment VARCHAR,
+    progress INTEGER,
+    length INTEGER,
+    link VARCHAR
 )
--- src/schema.rs
+++ src/schema.rs
@@ -4,5 +4,11 @@ diesel::table! {
     stories (id) {
         id -> Int4,
         title -> Varchar,
+        author -> Varchar,
+        rating -> Nullable<Int4>,
+        comment -> Nullable<Varchar>,
+        progress -> Nullable<Int4>,
+        length -> Nullable<Int4>,
+        link -> Nullable<Varchar>,
     }
 }
